c016940ada51faf6fa5b3e3107eda239e8688428
Gilang Ramadan <mgrware@gmail.com>
2022-02-18 18:23:01 +0700

    test#1 complete

--- tests/Feature/DebitCardControllerTest.php
+++ tests/Feature/DebitCardControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\DebitCard;
+use App\Models\DebitCardTransaction;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -26,7 +27,7 @@ class DebitCardControllerTest extends TestCase
         
         Passport::actingAs($this->user);
         
-        DebitCard::factory(2)
+        DebitCard::factory(10)
             ->create(["user_id" => $this->user->id]);
     }
 
@@ -44,7 +45,7 @@ class DebitCardControllerTest extends TestCase
                 'is_active'
             ]
         ]);
-        
+
         $this->assertDatabaseHas('debit_cards', ['user_id' => $this->user->id]);
 
     }
@@ -66,51 +67,133 @@ class DebitCardControllerTest extends TestCase
 
     public function testCustomerCanCreateADebitCard()
     {
-        $params = ["type" => ""];
+        $params = ['type' => ''];
         $response = $this->post('/api/debit-cards', $params);
         $response->assertStatus(HttpResponse::HTTP_FOUND);
         $this->assertDatabaseMissing('debit_cards', ['type' => 'test123']);
 
-        $params = ["type" => "test123"];
+        $params = ['type' => 'test123'];
         $response = $this->post('/api/debit-cards', $params);
-        $response->assertStatus(HttpResponse::HTTP_CREATED);
+        $response->assertStatus(HttpResponse::HTTP_CREATED)
+         ->assertJsonStructure([
+                'id',
+                'number',
+                'type',
+                'expiration_date',
+                'is_active'
+            ]);
         $this->assertDatabaseHas('debit_cards', ['type' => 'test123']);
 
     }
 
     public function testCustomerCanSeeASingleDebitCardDetails()
     {
-        // get api/debit-cards/{debitCard}
+        $debitCard = $this->user->debitCards->first();
+        $response = $this->get('/api/debit-cards/' . $debitCard->id);
+
+        $response->assertStatus(HttpResponse::HTTP_OK)
+            ->assertJsonStructure([
+                'id',
+                'number',
+                'type',
+                'expiration_date',
+                'is_active'
+            ]);
     }
 
     public function testCustomerCannotSeeASingleDebitCardDetails()
     {
         // get api/debit-cards/{debitCard}
+
+        DebitCard::factory(2)
+        ->create(['user_id' => $this->userOther->id]);
+        
+        Passport::actingAs($this->userOther);
+        
+        $debitCard = $this->user->debitCards->first();
+        $response = $this->get('/api/debit-cards/' . $debitCard->id);
+        $response->assertStatus(HttpResponse::HTTP_FORBIDDEN);
+      
+
     }
 
     public function testCustomerCanActivateADebitCard()
     {
         // put api/debit-cards/{debitCard}
+
+        $debitCard = $this->user->debitCards->first();
+        $params = ['is_active' => ''];
+        $response = $this->put('api/debit-cards/'. $debitCard->id, $params);
+        $response->assertStatus(HttpResponse::HTTP_FOUND);
+        $this->assertDatabaseMissing('debit_cards', ['disabled_at' => '']);
+
+        $params = ['is_active' => true];
+        $response = $this->put('api/debit-cards/'. $debitCard->id, $params);
+        $response->assertStatus(HttpResponse::HTTP_OK)
+        ->assertJsonStructure([
+            'id',
+            'number',
+            'type',
+            'expiration_date',
+            'is_active'
+        ]);
+        $this->assertDatabaseHas('debit_cards', ['disabled_at' => null]);
+    
     }
 
     public function testCustomerCanDeactivateADebitCard()
     {
-        // put api/debit-cards/{debitCard}
+        $debitCard = $this->user->debitCards->first();
+        $params = ['is_active' => ''];
+        $response = $this->put('api/debit-cards/'. $debitCard->id, $params);
+        $response->assertStatus(HttpResponse::HTTP_FOUND);
+        $this->assertDatabaseMissing('debit_cards', ['disabled_at' => '']);
+
+        $params = ['is_active' => false];
+        $response = $this->put('api/debit-cards/'. $debitCard->id, $params);
+        $response->assertStatus(HttpResponse::HTTP_OK)
+        ->assertJsonStructure([
+            'id',
+            'number',
+            'type',
+            'expiration_date',
+            'is_active'
+        ]);
+        $this->assertDatabaseMissing('debit_cards', ['id' => $debitCard->id, 'disabled_at' => null]);
     }
 
     public function testCustomerCannotUpdateADebitCardWithWrongValidation()
     {
-        // put api/debit-cards/{debitCard}
+        $debitCard = $this->user->debitCards->first();
+        $params = ['is_active' => '12356789'];
+        $response = $this->put('api/debit-cards/'. $debitCard->id, $params);
+        $response->assertStatus(HttpResponse::HTTP_FOUND);
+        $this->assertDatabaseMissing('debit_cards', ['id'=> $debitCard->id, 'disabled_at' => '12356789']);
+
     }
 
     public function testCustomerCanDeleteADebitCard()
     {
         // delete api/debit-cards/{debitCard}
+
+        $debitCard = $this->user->debitCards->first();
+        $response = $this->delete('api/debit-cards/' . $debitCard->id);
+        $response->assertStatus(HttpResponse::HTTP_NO_CONTENT);
+        $this->assertDatabaseMissing('debit_cards', ['id' => $debitCard->id, 'delete_at' => null]);
+
     }
 
     public function testCustomerCannotDeleteADebitCardWithTransaction()
     {
         // delete api/debit-cards/{debitCard}
+
+        $debitCard = $this->user->debitCards->first();
+        DebitCardTransaction::factory()->create(["debit_card_id" => $debitCard->id]);
+
+        $response = $this->delete('api/debit-cards/'. $debitCard->id);
+
+        $response->assertStatus(HttpResponse::HTTP_FORBIDDEN);
+        $this->assertDatabaseHas('debit_cards', ['id' => $debitCard->id]);
     }
 
     // Extra bonus for extra tests :)
